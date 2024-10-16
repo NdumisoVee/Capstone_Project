968f0c4472620eeae18c08044c2f952db874bc67
Ndumiso <ndumisovee@gmail.com>
2024-10-17 01:52:06 +0200

    - Updated ReviewDetailView to allow all users to view reviews, but only the author can update or delete their own reviews.

--- movie_review_api/reviews/models.py
+++ movie_review_api/reviews/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-
-
 class Movie(models.Model):
     title = models.CharField(max_length=255, unique=True)
-    description = models.TextField(blank=True, null=True)
-    release_date = models.DateField(blank=True, null=True)
+    description = models.TextField(default='No description available')
+    release_date = models.DateField(default='2000-01-01')
 
     def __str__(self):
         return self.title
@@ -22,18 +20,26 @@ class Review(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE,
                              related_name='reviews')  # ForeignKey to User ensures reviews are tied to specific users.
     created_date = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User, related_name='liked_reviews', blank=True)
 
     def __str__(self):
         return f"{self.movie_title} - {self.user.username}"
 
 
 class Like(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes')
-    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='likes')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='likes_for_review')
+    review = models.ForeignKey(Review, on_delete=models.CASCADE, related_name='likes_for_review')
     created_date = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        unique_together = ('user', 'review')
+        unique_together = ('user', 'review') # Ensures that a user can like a review only once
 
     def __str__(self):
         return f"{self.user.username} likes {self.review.movie.title}"
+
+
+class Comment(models.Model):
+    review = models.ForeignKey(Review, related_name='comments', on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
